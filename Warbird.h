39f033306c74e4fbae881341d9300f61073a91a0
raghavJhaRV <125516139+raghavJhaRV@users.noreply.github.com>
2023-04-11 00:49:46 -0400

    Update Warbird.h
    Signed-off-by: raghavJhaRV <125516139+raghavJhaRV@users.noreply.github.com>

--- Warbird.h
+++ Warbird.h
@@ -1,34 +1,40 @@
+#pragma once
 #ifndef WARBIRD_H
 #define WARBIRD_H
 
-#include "PropulsionSystem.h"
-#include "PayloadSystem.h"
-#include "WeaponSystem.h"
+#include "Propulsion.h"
+#include "Payload.h"
+#include "WeaponsSystem.h"
 
 #include <iostream>
 
 class Warbird
 {
 private:
-    PropulsionSystem *propulsionSystem1;
-    PropulsionSystem *propulsionSystem2;
-    PayloadSystem *payloadSystem;
-    WeaponSystem *weaponSystem1;
-    WeaponSystem *weaponSystem2;
-    int speed;            // speed of travel in meters per second
-    int distanceTraveled; // distance traveled in meters
+    Propulsion propulsionSystem1; // Why use pointers?
+    Propulsion propulsionSystem2;
+    Payload payloadSystem;
+    Weapons weaponSystem1;
+    Weapons weaponSystem2;
+    double speed;            // speed of travel in meters per second
+    double distanceTraveled; // distance traveled in meters
 
 public:
     Warbird();
-
-    Warbird(PropulsionSystem *ps1, PropulsionSystem *ps2, PayloadSystem *ps, WeaponSystem *ws1, WeaponSystem *ws2, int s)
-        : propulsionSystem1(ps1), propulsionSystem2(ps2), payloadSystem(ps), weaponSystem1(ws1), weaponSystem2(ws2), speed(s), distanceTraveled(0) {}
+    // constructor parameters are incorrect: refer to main file and assgn2
+    // constructor i am still working on it and a bit confused.
 
     int getSpeed() { return speed; }
     int getDistanceTraveled() { return distanceTraveled; }
 
     int setSpeed(int s) { speed = s; }
     int setDistanceTraveled(int d) { distanceTraveled = d; }
+
+    bool ChangeSpeed(double speed);
+    void Travel(double time, double lightLevel);
+    bool FireTorpedo(int torpedoes);
+    bool FireLaser();
+    void GenerateReport();
 };
 
 #endif // WARBIRD_H
